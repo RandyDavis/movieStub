ac64f4310aef810ac95c36639814514b4dfe2981
Randy Davis <randy_davis@outlook.com>
2014-10-23 02:12:46 -0500

    update index.js for use with express 4 and added the appropriate node modules as dependencies

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express"),
+    fs = require('fs'),
+    port = process.env.PORT || 2595,
+    bodyParser = require('body-parser'),
+    morgan = require('morgan');
+ 
+var app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.set("view options", {
+    layout: false
+});
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function (req, res) {
+    res.render('public/index.html');
+});
+ 
+app.listen(port);
+console.log('Express server running at http://localhost:' + port);
