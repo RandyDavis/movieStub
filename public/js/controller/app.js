ba0f9822d3640303be7c3c22eefc7a4402e4f824
Randy Davis <randy_davis@outlook.com>
2014-10-24 23:43:27 -0500

    update controller for bookTicketsController

--- public/js/controller/app.js
+++ public/js/controller/app.js
@@ -25,3 +25,27 @@ movieStubApp.controller("movieStubController", function ($scope, movieStubFactor
 movieStubApp.controller("movieDetailsController", function ($scope, $routeParams) {
     $scope.getMovieById($routeParams.id);
 });
+
+movie.movieStubApp.controller('bookTicketsController', function ($scope, $http, $location, $routeParams) {
+  $scope.getMovieById($routeParams.id);
+  $scope.onlyNumbers = /^\d+$/;
+  $scope.formData = {};
+  $scope.formData.movie_id = $scope.currMovie.id;
+  $scope.formData.movie_name = $scope.currMovie.name;
+  $scope.formData.date = "Today";
+
+  $scope.processForm = function() {
+    $http({
+        method: 'POST',
+        url: '/book',
+        data: $.param($scope.formData), // pass in data as strings
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        } // set the headers so angular passing info as form data (not request payload)
+    })
+        .success(function(data) {
+            // $Location.path("/bookings");
+            console.log(data);
+        });
+  };
+});
